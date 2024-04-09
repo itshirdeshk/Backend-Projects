5cb93b4cb4bb14eab95ad223e82f93f3ea8e4f37
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2024-04-09 23:05:17 +0530

    Done some mongoose changes!

--- Pin-Lite/routes/post.js
+++ Pin-Lite/routes/post.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/pin-lite");
-
 const postSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
