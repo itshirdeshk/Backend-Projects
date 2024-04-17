634e2fe53017df0c665171a1db0419c5d5e30e24
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2024-04-17 17:36:17 +0530

    Done some changes

--- Notes-Hub/app.js
+++ Notes-Hub/app.js
@@ -8,7 +8,7 @@ const passport = require("passport");
 const mongoStore = require("connect-mongo");
 
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(
     session({
